c33b2de27713e536bfaf9a01019d18884e3e75d4
Sunmen <921013lxw>
2018-09-03 22:18:42 +0800

    finish walking page

--- app/src/main/java/com/siat/hmc/intellibot/MainActivity.java
+++ app/src/main/java/com/siat/hmc/intellibot/MainActivity.java
@@ -1,7 +1,7 @@
 package com.siat.hmc.intellibot;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.PagerAdapter;
@@ -11,26 +11,20 @@ import android.support.v7.widget.Toolbar;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.view.animation.LinearInterpolator;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Scroller;
 import android.widget.TextView;
 
 import com.siat.hmc.intellibot.iface.FuncSelectInterface;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements FuncSelectInterface {
 
@@ -77,13 +71,11 @@ public class MainActivity extends AppCompatActivity implements FuncSelectInterfa
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
         toolbar.setTitle(R.string.welcome);
 
         mViewPager = (ViewPager) findViewById(R.id.container);
 
-        flist.add(new Func("Guide Walking", R.mipmap.guide_walking));
-        flist.add(new Func("Random Working", R.mipmap.random_working));
+        flist.add(new Func("Walking Control", R.mipmap.guide_walking));
         flist.add(new Func("Voice Reading", R.mipmap.voice_reading));
         flist.add(new Func("Language Learning", R.mipmap.language_learning));
         flist.add(new Func("Setting", R.mipmap.setting));
@@ -120,7 +112,14 @@ public class MainActivity extends AppCompatActivity implements FuncSelectInterfa
 
     @Override
     public void selectFunc(int position) {
-        System.out.println("click:" + position);
+        switch(position) {
+            case 0:
+                Intent intent=new Intent(MainActivity.this, ControlActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
